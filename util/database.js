c920aec6095b1a46619be26f44cc49084efa4f2e
Ananya <asingh2@Cogitate.us>
2023-05-11 19:19:45 +0530

    ref #100 - initialized project

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'password@as1'
+})
+
+module.exports = pool.promise();
